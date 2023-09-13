a0649653ed91101e2de6ae505e516af665fa1096
Ammar <33799463+ammar629@users.noreply.github.com>
2023-09-13 21:05:18 +0500

    Added ElevatedButtonWidget class

--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,27 @@ class HomePage extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
-        title: Title(color: Colors.red, child: Text('Codegitz - Example')),
-        centerTitle: true,
-        elevation: 10,
+      title: Text('Codegitz - Customized Button Widget Example'),
+      centerTitle: true,
+      elevation: 10,
+      backgroundColor: Colors.red[600],
       ),
       body: Center(
+        child: ElevatedButtonWidget(),
       ),
+    );
+  }
+}
+
+class ElevatedButtonWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return ElevatedButton(onPressed: null, 
+    child: Text('ElevatedButton'),
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.red[600]),
+      foregroundColor: MaterialStateProperty.all(Colors.white),
+    ) 
     );
   }
 }
